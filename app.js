94e1710000be78483b0e08f0b898dfb046967843
Ted Irland <irlandth@gmail.com>
2021-05-02 11:42:38 -0500

    Pushing with driver create route fully functional

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express');
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 //Watch for incoming requests of method get
 //to the route http://localhost:3050/api
+
+mongoose.connect('mongodb://localhost/muber', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 app.use(bodyParser.json());
 routes(app);
 
