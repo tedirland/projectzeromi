0e82b29543293e00be841ed4257145560ba705cd
Ted Irland <irlandth@gmail.com>
2021-05-02 06:34:41 -0500

    Pushing with mocha test written to verify get route is working as intended

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = require('./routes/routes');
+const bodyParser = require('body-parser');
 const app = express();
 //Watch for incoming requests of method get
 //to the route http://localhost:3050/api
-
+app.use(bodyParser.json());
 routes(app);
 
 module.exports = app;
